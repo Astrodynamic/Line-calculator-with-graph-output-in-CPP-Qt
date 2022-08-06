31064d8a5d4d7d9da2819e0aaa8c56b6ebdff0e1
Werewolf Jane <werewolf@ob-c1.nsk.21-school.ru>
2022-08-06 23:05:04 +0700

    change cmake

--- build-src-Desktop_x86_darwin_generic_mach_o_64bit-Debug/qtcsettings.cmake
+++ build-src-Desktop_x86_darwin_generic_mach_o_64bit-Debug/qtcsettings.cmake
@@ -1,9 +1,2 @@
 # This file is managed by Qt Creator, do not edit!
 
-set("CMAKE_GENERATOR" "Unix Makefiles" CACHE "STRING" "" FORCE)
-set("CMAKE_BUILD_TYPE" "Debug" CACHE "STRING" "" FORCE)
-set("CMAKE_PROJECT_INCLUDE_BEFORE" "/opt/goinfre/werewolf/Qt Creator.app/Contents/Resources/package-manager/auto-setup.cmake" CACHE "FILEPATH" "" FORCE)
-set("QT_QMAKE_EXECUTABLE" "/usr/local/Cellar/qt/6.2.3/bin/qmake6" CACHE "FILEPATH" "" FORCE)
-set("CMAKE_PREFIX_PATH" "/usr/local" CACHE "PATH" "" FORCE)
-set("CMAKE_C_COMPILER" "/usr/bin/clang" CACHE "FILEPATH" "" FORCE)
-set("CMAKE_CXX_COMPILER" "/usr/bin/clang++" CACHE "FILEPATH" "" FORCE)
