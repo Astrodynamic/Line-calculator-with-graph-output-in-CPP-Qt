3d6e74e554b51398e0547c840dd00287fcc179c0
Werewolf Jane <werewolf@ob-c1.nsk.21-school.ru>
2022-08-08 21:47:48 +0700

    add map

--- src/include/calculation.hpp
+++ src/include/calculation.hpp
@@ -25,30 +25,30 @@ class Calculation {
   enum class e_farg_t { DEFAULT, F_1ARG, F_2ARG };
   enum class e_priority_t {DEFAULT, L_PR, M_PR, H_PR, UNARY, FUNC};
 
-  QMap<QChar, QPair<int, QPair<int, double (*)()>>> m_fun_ptr{
+  QMap<QChar, QPair<e_priority_t, QPair<e_farg_t, std::variant<std::function<double(double, double)>, std::function<double(double)>, nullptr_t >>>> m_fun_ptr {
       {'(', {e_priority_t::DEFAULT, {e_farg_t::DEFAULT, nullptr}}},
-      {'+', {e_priority_t::L_PR, {e_farg_t::F_2ARG, [](double lhs, double rhs) {return lhs + rhs;}}}},
-      {'-', {e_priority_t::L_PR, {e_farg_t::F_2ARG, [](double lhs, double rhs) {return lhs - rhs;}}}},
-      {'*', {e_priority_t::M_PR, {e_farg_t::F_2ARG, [](double lhs, double rhs) {return lhs * rhs;}}}},
-      {'/', {e_priority_t::M_PR, {e_farg_t::F_2ARG, [](double lhs, double rhs) {return lhs / rhs;}}}},
-      {'A', {e_priority_t::M_PR, {e_farg_t::F_2ARG, std::fmod}}},
-      {'^', {e_priority_t::H_PR, {e_farg_t::F_2ARG, std::pow}}},
+      {'+', {e_priority_t::L_PR, {e_farg_t::F_2ARG, [](double lhs, double rhs) -> double {return lhs + rhs;}}}},
+      {'-', {e_priority_t::L_PR, {e_farg_t::F_2ARG, [](double lhs, double rhs) -> double {return lhs - rhs;}}}},
+      {'*', {e_priority_t::M_PR, {e_farg_t::F_2ARG, [](double lhs, double rhs) -> double {return lhs * rhs;}}}},
+      {'/', {e_priority_t::M_PR, {e_farg_t::F_2ARG, [](double lhs, double rhs) -> double {return lhs / rhs;}}}},
+      {'A', {e_priority_t::M_PR, {e_farg_t::F_2ARG, (double(*)(double, double))&std::fmod}}},
+      {'^', {e_priority_t::H_PR, {e_farg_t::F_2ARG, (double(*)(double, double))&std::pow}}},
       {'B', {e_priority_t::UNARY, {e_farg_t::DEFAULT, nullptr}}},
-      {'C', {e_priority_t::UNARY, {e_farg_t::F_1ARG, [](double a) {return -a;}}}},
-      {'D', {e_priority_t::FUNC, {e_farg_t::F_1ARG, std::cos}}},
-      {'E', {e_priority_t::FUNC, {e_farg_t::F_1ARG, std::sin}}},
-      {'F', {e_priority_t::FUNC, {e_farg_t::F_1ARG, std::tan}}},
-      {'G', {e_priority_t::FUNC, {e_farg_t::F_1ARG, std::acos}}},
-      {'H', {e_priority_t::FUNC, {e_farg_t::F_1ARG, std::asin}}},
-      {'I', {e_priority_t::FUNC, {e_farg_t::F_1ARG, std::atan}}},
-      {'J', {e_priority_t::FUNC, {e_farg_t::F_1ARG, std::sqrt}}},
-      {'K', {e_priority_t::FUNC, {e_farg_t::F_1ARG, std::log}}},
-      {'L', {e_priority_t::FUNC, {e_farg_t::F_1ARG, std::log10}}}
+      {'C', {e_priority_t::UNARY, {e_farg_t::F_1ARG, [](double a) -> double {return -a;}}}},
+      {'D', {e_priority_t::FUNC, {e_farg_t::F_1ARG, (double(*)(double))&std::cos}}},
+      {'E', {e_priority_t::FUNC, {e_farg_t::F_1ARG, (double(*)(double))&std::sin}}},
+      {'F', {e_priority_t::FUNC, {e_farg_t::F_1ARG, (double(*)(double))&std::tan}}},
+      {'G', {e_priority_t::FUNC, {e_farg_t::F_1ARG, (double(*)(double))&std::acos}}},
+      {'H', {e_priority_t::FUNC, {e_farg_t::F_1ARG, (double(*)(double))&std::asin}}},
+      {'I', {e_priority_t::FUNC, {e_farg_t::F_1ARG, (double(*)(double))&std::atan}}},
+      {'J', {e_priority_t::FUNC, {e_farg_t::F_1ARG, (double(*)(double))&std::sqrt}}},
+      {'K', {e_priority_t::FUNC, {e_farg_t::F_1ARG, (double(*)(double))&std::log}}},
+      {'L', {e_priority_t::FUNC, {e_farg_t::F_1ARG, (double(*)(double))&std::log10}}}
       };
 
  public:
   Calculation();
-
+  
   void create_exp(QString& infix);
 };
 
